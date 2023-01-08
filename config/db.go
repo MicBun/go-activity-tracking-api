a3db15cc4245f2e33e6c106f156c7efcb163ebf5
MicBun <michael.boentarman@sirclo.com>
2023-01-09 05:00:25 +0700

    use postgres for render

--- config/db.go
+++ config/db.go
@@ -13,8 +13,9 @@ func ConnectDataBase() *gorm.DB {
 	//database := utils.GetEnv("DB_DATABASE", "activity_tracking")
 	//dsn := username + ":" + password + "@" + host + "/" + database + "?charset=utf8mb4&parseTime=True&loc=Local"
 	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	dsn := "host=dpg-cetjjlpgp3jmgl18q2rg-a user=myuser password=yN5Do2dZnQivCkSqulOK9mwWa6VPB5Dx dbname=activity_tracking port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	//dsn := "host=dpg-cetjjlpgp3jmgl18q2rg-a user=myuser password=yN5Do2dZnQivCkSqulOK9mwWa6VPB5Dx dbname=activity_tracking port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	dbUrl := "postgres://myuser:yN5Do2dZnQivCkSqulOK9mwWa6VPB5Dx@dpg-cetjjlpgp3jmgl18q2rg-a/activity_tracking"
+	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
