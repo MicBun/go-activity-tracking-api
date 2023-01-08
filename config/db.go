48bce5c63a550cf7eb11dd1331c081aa0fccc3ab
MicBun <michael.boentarman@sirclo.com>
2023-01-09 05:27:59 +0700

    modify readme

--- config/db.go
+++ config/db.go
@@ -7,15 +7,21 @@ import (
 )
 
 func ConnectDataBase() *gorm.DB {
+	////////
+	// Uncomment this part to use docker
 	//username := utils.GetEnv("DB_USERNAME", "myuser")
 	//password := utils.GetEnv("DB_PASSWORD", "mypassword")
 	//host := utils.GetEnv("DB_HOST", "tcp(mysql:3306)")
 	//database := utils.GetEnv("DB_DATABASE", "activity_tracking")
 	//dsn := username + ":" + password + "@" + host + "/" + database + "?charset=utf8mb4&parseTime=True&loc=Local"
 	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	//dsn := "host=dpg-cetjjlpgp3jmgl18q2rg-a user=myuser password=yN5Do2dZnQivCkSqulOK9mwWa6VPB5Dx dbname=activity_tracking port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	////////
+
+	/////// comment this part to use docker
 	dbUrl := "postgres://myuser:yN5Do2dZnQivCkSqulOK9mwWa6VPB5Dx@dpg-cetjjlpgp3jmgl18q2rg-a/activity_tracking"
 	db, err := gorm.Open(postgres.Open(dbUrl), &gorm.Config{})
+	///////
+
 	if err != nil {
 		panic(err)
 	}
